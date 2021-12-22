f61f5d6ed3f32aa1d3dd7925da1696809330f0c9
RahulP0369 <rp98514360@gmail.com>
2021-12-22 13:36:13 +0530

    Write a program which contains one function named as Fun(). That function should display

---
+++ Assignment1.1.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Function Name :  fun()  
+#
+# Description   :   Write a program which contains one function named as Fun(). 
+#                   That function should display “Hello from Fun” on console.
+#
+# Input         :  String
+#
+# Output        :  String
+#
+# Author        :  Rahul Patil
+##############################################################################
+
+def fun():
+    
+    print("Hello World")
+
+def main():
+
+    fun()
+
+if __name__ == "__main__":
+    main()
